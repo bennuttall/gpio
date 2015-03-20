7db345f93f1aede89d2e2aa921184bb2f4b4975f
Ben Nuttall <ben@bennuttall.com>
2015-03-20 21:26:06 +0000

    Tidy up, add docstrings

--- gpio/outputs.py
+++ gpio/outputs.py
@@ -27,11 +27,22 @@ PINS = [
         21
 ]
 
+
 def gpio_setup():
+    """
+    Setup all GPIO pins in the list as outputs.
+    """
+
     for pin in PINS:
         GPIO.setup(pin, GPIO.OUT)
 
+
 def all_on(step=0, reverse=False):
+    """
+    Set each GPIO pin to high in sequence (reversed if `reverse` is True) with
+    a pause between each one of a number of seconds given in `step`.
+    """
+
     pins = reversed(PINS) if reverse else PINS
 
     for pin in pins:
@@ -39,13 +50,20 @@ def all_on(step=0, reverse=False):
         print("pin %2.0f on" % pin)
         sleep(step)
 
+
 def all_off(step=0, reverse=False):
+    """
+    Set each GPIO pin to low in sequence (reversed if `reverse` is True) with
+    a pause between each one of a number of seconds given in `step`.
+    """
+
     pins = reversed(PINS) if reverse else PINS
 
     for pin in pins:
         GPIO.output(pin, False)
         print("pin %2.0f off" % pin)
         sleep(step)
+
 
 def main():
     gpio_setup()
